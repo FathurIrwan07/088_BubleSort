8de49c192e27a7180547bf6e33a5c9e28ac7e6c4
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-03-17 16:45:54 +0700

    manambahakan prosedur untuk menampilkan elemen araay yg belum tersusun

--- BubleSort/BubleSort.cpp
+++ BubleSort/BubleSort.cpp
@@ -44,4 +44,11 @@ void unsorted() {
     for (int j = 0; j < n; j++) {                       //looping dengan j dimulai dari 0 hingga n-1
         cout << a[j] << endl;                           //output ke layar
     }
+    cout << endl;                                       //output baris kosong
 }
+
+
+
+
+
+
