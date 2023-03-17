c7ae94989a9cf35fadf71ebc41cb1795b773c669
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-03-17 16:42:11 +0700

    manambahkan prosedur untuk menampilkan elemen araay  yg belum tersusun

--- BubleSort/BubleSort.cpp
+++ BubleSort/BubleSort.cpp
@@ -36,4 +36,12 @@ void bubbleSortAraay() {               //prosedur untuk menggurutkan array denga
     }
 }
 
-
+void unsorted() {
+    cout << endl;                                      //output barais kosong
+    cout << "==============================" << endl;   //output ke layar
+    cout << "Elemen Araay yg belum tersusun" << endl;   //output ke layar
+    cout << "==============================" << endl;   //output ke layar
+    for (int j = 0; j < n; j++) {                       //looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                           //output ke layar
+    }
+}
